17b998783a65a2f8c2778680b97483091b4f0969
glaslan <glasd6702@gmail.com>
2024-02-25 22:28:54 -0500

    Implemented file output

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 import {Notice, Plugin} from 'obsidian'
+import { promises as fsPromises } from 'fs';
+import { join } from 'path';
 
 export default class TanglePlugin extends Plugin{
-	statusBarTextElement: HTMLSpanElement;
 
 	onload() {
 		// This code adds a icon to the left vertical ribbon that dispays a 
@@ -10,13 +11,13 @@ export default class TanglePlugin extends Plugin{
 			const file = this.app.workspace.getActiveFile();
 			if(file){
 				const content = await this.app.vault.read(file);
-				this.getCodeBlocks(content);
+				let code : string = this.getCodeBlocks(content);
+				let extension : string = this.getCodeExtension(content);
+				this.writeCodeToFile("", file.basename, extension, code);
 			}
 			
 		});
 		console.log("tangle is loaded");
-		this.statusBarTextElement = this.addStatusBarItem().createEl('span');
-		this.statusBarTextElement.textContent = "Hello";
 
 		this.app.workspace.on('active-leaf-change', async () => {
 			const file = this.app.workspace.getActiveFile();
@@ -32,14 +33,39 @@ export default class TanglePlugin extends Plugin{
 		console.log('Unloading Tangle')
 	}
 
-	private getCodeBlocks(fileContent?: string){
-		let code : string = "";
+	private async writeCodeToFile(path: string, filename: string, extension: string, data: string) {
+		let file: string = filename + "." + extension;
+		try {
+			await fsPromises.writeFile(join(path, file), data, {
+			  flag: 'w',
+			});
+		
+			const contents = await fsPromises.readFile(
+			  join(path, file),
+			  'utf-8',
+			);
+			console.log(contents);
+		
+			return contents;
+		  } catch (err) {
+			console.log(err);
+			return 'Something went wrong while writing code to file';
+		  }
+	}
+
+	private getCodeExtension(fileContent?: string){
 		let codeExtension : string = "";
 		const rExpFileExtension : RegExp = /(?<=^```)(\w)/gm;
-		const rExp : RegExp = /(?<=^```\w\s)([^```]*)/gm;
 		codeExtension += fileContent?.match(rExpFileExtension);
-		code += fileContent?.match(rExp)?.join("");
 		console.log(codeExtension);
+		return codeExtension[0].toLowerCase();
+	}
+
+	private getCodeBlocks(fileContent?: string){
+		let code : string = "";
+		const rExp : RegExp = /(?<=^```\w\s)([^```]*)/gm;
+		code += fileContent?.match(rExp)?.join("");
 		console.log(code);
+		return code;
 	}
 }
